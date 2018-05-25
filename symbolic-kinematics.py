9533de955be5d5e60c60b79031f2895008817ad6
Oskar Henriksson <o.henriksson@gmail.com>
2018-05-25 19:13:11 +0200

    add theta_4 calc

--- symbolic-kinematics.py
+++ symbolic-kinematics.py
@@ -116,7 +116,7 @@ def solveTheta3(Pmat2, T16):
 
     # pprint(simplify(eq1 - eq2 -eq3 ))
 
-def solveTheta2(T03, T06, T36, theta3):
+def solveTheta2(T03, T36, theta3):
     print('-----theta2-------')
     leftM = MyInv(T03)*createPosMatrix()
     l14 = leftM.row(0).col(3)[0]
@@ -134,7 +134,31 @@ def solveTheta2(T03, T06, T36, theta3):
     return theta2
 
 
-def solveTheta5(T04, T06, T46, theta1, theta2, theta3, theta4):
+def solveTheta4(T03, T36, theta1,theta2,theta3):
+    print('-----theta4-------')
+    leftM = MyInv(T03)*createPosMatrix()
+    c = 2
+    l13 = leftM.row(0).col(c)[0]
+    r13 = T36.row(0).col(c)[0]
+    l33 = leftM.row(2).col(c)[0]
+    r33 = T36.row(2).col(c)[0]
+    eq1 = l13-r13
+    eq2 = l33-r33
+    pprint(eq1)
+    pprint(eq2)
+
+    c4 = solve(eq1, cos(t(4))*sin(t(5)))[0]
+    s4 = solve(eq2, sin(t(4))*sin(t(5)))[0]
+
+    #assume s5!=0 #singularity
+    theta4 = atan2(c4,s4)
+    theta4 = 0
+    pprint(theta4)
+    theta4b = theta4 + pi
+    return theta4
+
+
+def solveTheta5(T04, T46, theta1, theta2, theta3, theta4):
     print('-----theta5-------')
     leftM = MyInv(T04)*createPosMatrix()
     c = 2
@@ -145,11 +169,11 @@ def solveTheta5(T04, T06, T46, theta1, theta2, theta3, theta4):
     s5 = solve(l13-r13,sin(t(5)))[0]
     c5 = solve(l33-r33,cos(t(5)))[0]
     theta5 = atan2(s5,c5)
-    pprint(theta5)
+    theta5b = -theta5
     return theta5
 
 
-def solveTheta6(T05, T06, T56, theta1, theta2, theta3, theta4, theta5):
+def solveTheta6(T05, T56, theta1, theta2, theta3, theta4, theta5):
     print('-----theta6-------')
     leftM = MyInv(T05)*createPosMatrix()
     c = 0
@@ -160,7 +184,7 @@ def solveTheta6(T05, T06, T56, theta1, theta2, theta3, theta4, theta5):
     c6 = solve(l11-r11,cos(t(6)))[0]
     s6 = solve(l31-r31,sin(t(6)))[0]
     theta6 = atan2(s6,c6)
-    pprint(theta6)
+    theta6b = theta6 + pi
     return theta6
 
 
@@ -189,13 +213,17 @@ T03 = T1*T2*T3
 T36 = T4*T5*T6
 # solveTheta2(T03,T06,T36,2)
 
+T03 = T1*T2*T3
+T36 = T4*T5*T6
+solveTheta4(T03,T36,'t1','t2','t3')
+
 # T04 = T1*T2*T3*T4
 # T46 = T5*T6
 # solveTheta5(T04,T06,T46,'t1','t2','t3','t4')
 
-T05 = T1*T2*T3*T4*T5
-T56 = T6
-solveTheta6(T05,T06,T56,'t1','t2','t3','t4', 't5')
+# T05 = T1*T2*T3*T4*T5
+# T56 = T6
+# solveTheta6(T05,T56,'t1','t2','t3','t4', 't5')
 # T16a = T1.inv()*(T1*T2*T3*T4*T5*T6)
 # T16b = T2*T3*T4*T5*T6
 # cella =(T16a.col(3).row(1))
